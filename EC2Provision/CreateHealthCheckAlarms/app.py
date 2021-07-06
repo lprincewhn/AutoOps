fa1d0e712db19b95fcd97d7f90b1006f78eefc7c
Wang <whuaning@a483e791ddd1.ant.amazon.com>
2021-07-06 11:46:36 +0800

    Add statuscheck alarms

--- EC2Provision/CreateHealthCheckAlarms/app.py
+++ EC2Provision/CreateHealthCheckAlarms/app.py
@@ -46,22 +46,43 @@ def lambda_handler(event, context):
             Tags=[]
         )            
         print(f'Response: {response}')
-    response = client.put_metric_alarm(
-        AlarmName=f'EC2-{event["InstanceId"]}-Failed-InstanceStatusCheck-Alarm',
-        MetricName='StatusCheckFailed_Instance',
-        Namespace='AWS/EC2',
-        Statistic='Average',
-        Dimensions=[{
-            'Name': 'InstanceId',
-            'Value': event['InstanceId']
-        }],
-        Period=60,
-        EvaluationPeriods=3,
-        DatapointsToAlarm=3,
-        Threshold=0,
-        ComparisonOperator='GreaterThanThreshold',
-        TreatMissingData='missing',
-        Tags=[]
-    )
-    print(f'Response: {response}')
+    try:
+        response = client.put_metric_alarm(
+            AlarmName=f'EC2-{event["InstanceId"]}-Failed-InstanceStatusCheck-Alarm',
+            AlarmActions=[f'arn:aws:automate:{region}:ec2:reboot'],
+            MetricName='StatusCheckFailed_Instance',
+            Namespace='AWS/EC2',
+            Statistic='Average',
+            Dimensions=[{
+                'Name': 'InstanceId',
+                'Value': event['InstanceId']
+            }],
+            Period=60,
+            EvaluationPeriods=3,
+            DatapointsToAlarm=3,
+            Threshold=0,
+            ComparisonOperator='GreaterThanThreshold',
+            TreatMissingData='missing',
+            Tags=[]
+        )
+        print(f'Response: {response}')
+    except:
+        response = client.put_metric_alarm(
+            AlarmName=f'EC2-{event["InstanceId"]}-Failed-InstanceStatusCheck-Alarm',
+            MetricName='StatusCheckFailed_Instance',
+            Namespace='AWS/EC2',
+            Statistic='Average',
+            Dimensions=[{
+                'Name': 'InstanceId',
+                'Value': event['InstanceId']
+            }],
+            Period=60,
+            EvaluationPeriods=3,
+            DatapointsToAlarm=3,
+            Threshold=0,
+            ComparisonOperator='GreaterThanThreshold',
+            TreatMissingData='missing',
+            Tags=[]
+        )
+        print(f'Response: {response}')
     return event
--- deploy.sh
+++ deploy.sh
@@ -1,36 +1,8 @@
-cd Common/
-MAIN_REGION=us-east-1
-sam build
-sam deploy --stack-name AutoOpsCommon --region $MAIN_REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-SNS_TOPIC_ARN=$(aws cloudformation describe-stacks --stack-name AutoOpsCommon --region $MAIN_REGION --no-cli-pager --query 'Stacks[0].Outputs[?OutputKey==`SNSTopic`].OutputValue' --output text)
-cd ..
-
 REGIONS=(us-east-1)
-cd AlarmProcessor
-for REGION in ${REGIONS}
-do 
-  sam build
-  sam deploy --stack-name AutoOpsAlarmProcessor --region $REGION --parameter-overrides SnsTopicArn=$SNS_TOPIC_ARN --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-done
-cd ..
 cd EC2Provision
 for REGION in ${REGIONS}
 do
-  sam build
+  sam build --use-container
   sam deploy --stack-name AutoOpsEC2Provision --region $REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-done
-cd ..
-cd RDSProvision
-for REGION in ${REGIONS}
-do
-  sam build
-  sam deploy --stack-name AutoOpsRDSProvision --region $REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-done
-cd ..
-cd ESProvision
-for REGION in ${REGIONS}
-do
-  sam build
-  sam deploy --stack-name AutoOpsESProvision --region $REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
 done
 cd ..
