c22157e89d574743331861e7d2b8db0e3b297811
lprincewhn <lprincewhn@gmail.com>
2021-07-09 08:40:52 +0800

    1. Add script to deploy with container.

--- deploy_with_container.sh
+++ deploy_with_container.sh
@@ -1,43 +1,9 @@
-cd Common/
-MAIN_REGION=us-east-1
-sam build --use-container
-sam deploy --stack-name AutoOpsCommon --region $MAIN_REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-SNS_TOPIC_ARN=$(aws cloudformation describe-stacks --stack-name AutoOpsCommon --region $MAIN_REGION --no-cli-pager --query 'Stacks[0].Outputs[?OutputKey==`SNSTopic`].OutputValue' --output text)
-cd ..
 
 REGIONS=(us-east-1)
-cd AlarmProcessor
-for REGION in ${REGIONS}
-do 
-  sam build --use-container
-  sam deploy --stack-name AutoOpsAlarmProcessor --region $REGION --parameter-overrides SnsTopicArn=$SNS_TOPIC_ARN --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-done
-cd ..
-cd PhdEventProcessor
-for REGION in ${REGIONS}
-do
-  sam build --use-container
-  sam deploy --stack-name AutoOpsPhdEventProcessor --region $REGION --parameter-overrides SnsTopicArn=$SNS_TOPIC_ARN --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-done
-cd ..
 cd EC2Provision
 for REGION in ${REGIONS}
 do
   sam build --use-container
-  sam deploy --stack-name AutoOpsEC2Provision --region $REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-done
-cd ..
-cd RDSProvision
-for REGION in ${REGIONS}
-do
-  sam build --use-container
-  sam deploy --stack-name AutoOpsRDSProvision --region $REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
-done
-cd ..
-cd ESProvision
-for REGION in ${REGIONS}
-do
-  sam build --use-container
-  sam deploy --stack-name AutoOpsESProvision --region $REGION --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
+  sam deploy --stack-name AutoOpsEC2Provision --region $REGION --parameter-overrides SnsTopicArn=$1 --confirm-changeset --resolve-s3 --capabilities CAPABILITY_IAM
 done
 cd ..
