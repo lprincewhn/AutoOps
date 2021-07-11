99631ef60ac976506d6b9a63b40cc53b05907bf7
lprincewhn <lprincewhn@gmail.com>
2021-07-11 22:24:40 +0800

    1. Fix permission and format problem.

--- PhdEventProcessor/FormatEC2MaintenanceEvent/app.py
+++ PhdEventProcessor/FormatEC2MaintenanceEvent/app.py
@@ -24,15 +24,15 @@ def lambda_handler(event, context):
             tags_on_ec2 = i.get('Tags', [])
             nametag = list(filter(lambda x:x.get('Key')=='Name', tags_on_ec2))
             instanceName = nametag[0].get('Value') if nametag else '-'
-            resourceNameStr += f'{instanceName} ({i.get("InstanceId")}, {i.get("PrivateIpAddress")}\n'
+            resourceNameStr += f'{instanceName} ({i.get("InstanceId")}, {i.get("PrivateIpAddress")}),'
     message = None
     if 'AWS_EC2_INSTANCE_REBOOT_FLEXIBLE_MAINTENANCE_SCHEDULED' in eventTypeCode:
-        message = f'''
-            时间: {timestamp}
-            AWS区域：{region}
-            资源类型：EC2实例
-            资源名称：{resourceNameStr}事件：EC2维护重启
-            详情：EC2实例在维护期间将会不可用并自动重启。如果该实例不带实例存储，你可以在{timestamp}之前手动进行Stop/Start操作，该操作将会使得实例被迁移到健康主机上。{eventDescription}
+        message = f'''时间: {timestamp}
+AWS区域：{region}
+资源类型：EC2实例
+资源名称：{resourceNameStr[:-1]}
+事件：EC2维护重启
+详情：EC2实例在维护期间将会不可用并自动重启。如果该实例不带实例存储，你可以在{timestamp}之前手动进行Stop/Start操作，该操作将会使得实例被迁移到健康主机上。{eventDescription}
 '''
     if message:
         event['message'] = message
