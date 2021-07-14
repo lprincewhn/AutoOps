b645342c7a113489791697e42379feb54575ff98
lprincewhn <lprincewhn@gmail.com>
2021-07-14 11:12:39 +0800

    Send moer EC2 notification

--- EC2Provision/LoadEC2ProvisionInput/app.py
+++ EC2Provision/LoadEC2ProvisionInput/app.py
@@ -1,8 +1,16 @@
-import string
 import datetime
 
 def lambda_handler(event, context):
     print(f'Input: {event}')
+    beijing_time = datetime.datetime.strptime(event["time"], '%Y-%m-%dT%H:%M:%SZ').astimezone(tz =datetime.timezone
+    (datetime.timedelta(hours=8)))
     instancdId = event.get('detail').get('instance-id')
     state = event.get('detail').get('state')
-    return {'InstanceId': instancdId, 'State': state}
+    return {
+        'account': event['account'],
+        'region': event['region'],
+        'timestamp': datetime.datetime.strftime(beijing_time, '%Y-%m-%dT%H:%M:%S+0800'), 
+        'time': datetime.datetime.strftime(beijing_time, '%H:%M:%S'),
+        'InstanceId': instancdId, 
+        'State': state
+    }
