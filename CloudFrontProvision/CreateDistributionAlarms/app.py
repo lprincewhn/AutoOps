93010145a0553ea8cf06e666f0e48a41408eb85a
lprincewhn <lprincewhn@gmail.com>
2022-04-25 12:02:08 +0800

    update alarms

--- CloudFrontProvision/CreateDistributionAlarms/app.py
+++ CloudFrontProvision/CreateDistributionAlarms/app.py
@@ -9,7 +9,16 @@ logger.setLevel(logging.DEBUG if os.getenv("DEBUG", None) else logging.INFO)
 
 def lambda_handler(event, context):
     logger.info(f'Event In: {event}')
-    client = ec2 = boto3.client('cloudwatch')
+    client = boto3.client('cloudfront')
+    response = client.create_monitoring_subscription(
+        DistributionId=event['DistributionId'],
+        MonitoringSubscription={
+            'RealtimeMetricsSubscriptionConfig': {
+                'RealtimeMetricsSubscriptionStatus': 'Enabled'
+            }
+        }
+    )
+    client = boto3.client('cloudwatch')
     response = client.put_metric_alarm(
         AlarmName=f'CloudFront-{event["DistributionId"]}-High-5xxErrorRate-Alarm',
         ActionsEnabled=False,
@@ -27,83 +36,6 @@ def lambda_handler(event, context):
         EvaluationPeriods=2,
         DatapointsToAlarm=2,
         Threshold=5,
-        ComparisonOperator='GreaterThanOrEqualToThreshold',
-        TreatMissingData='missing',
-        Tags=[]
-    )
-    logger.info(f'Response: {response}')
-    response = client.put_metric_alarm(
-        AlarmName=f'CloudFront-{event["DistributionId"]}-High-OriginBandwidth-Alarm',
-        ActionsEnabled=False,
-        Metrics=[{
-            'Id': 'origin_bandwidth',
-            'Expression': 'byte_downloaded*(100-cache_hit_rate)/100*8/PERIOD(byte_downloaded)',
-            'Label': 'OriginBandwidth(bps)',
-            'ReturnData': True
-        },{
-            'Id': 'byte_downloaded',
-            'MetricStat': {
-            	'Metric': {
-                    'Namespace': 'AWS/CloudFront',
-                    'MetricName': 'BytesDownloaded',
-                    'Dimensions': [{
-                        'Name': 'Region',
-                        'Value': 'Global'
-                    },{
-                        'Name': 'DistributionId',
-                        'Value': event["DistributionId"]
-                    }]
-                },
-                'Period': 300,
-                'Stat': 'Sum' 
-            },
-            'Label': 'BytesDownloaded',
-            'ReturnData': False 
-        },{
-            'Id': 'cache_hit_rate',
-            'MetricStat': {
-                'Metric': {
-                    'Namespace': 'AWS/CloudFront',
-                    'MetricName': 'CacheHitRate',
-                    'Dimensions': [{
-                        'Name': 'Region',
-                        'Value': 'Global'
-                    },{
-                        'Name': 'DistributionId',
-                        'Value': event["DistributionId"]
-                    }]
-                },
-                'Period': 300,
-                'Stat': 'Average'
-            },
-            'Label': 'CacheHitRate',
-            'ReturnData': False
-        }],
-        EvaluationPeriods=1,
-        DatapointsToAlarm=1,
-        Threshold=1500000000,
-        ComparisonOperator='GreaterThanOrEqualToThreshold',
-        TreatMissingData='missing',
-        Tags=[]
-    )
-    logger.info(f'Response: {response}')
-    response = client.put_metric_alarm(
-        AlarmName=f'CloudFront-{event["DistributionId"]}-High-Requests-Alarm',
-        ActionsEnabled=False,
-        MetricName='Requests',
-        Namespace='AWS/CloudFront',
-        Statistic='Sum',
-        Dimensions=[{
-            'Name': 'Region',
-            'Value': 'Global'
-        },{
-            'Name': 'DistributionId',
-            'Value': event["DistributionId"]
-        }],
-        Period=300,
-        EvaluationPeriods=1,
-        DatapointsToAlarm=1,
-        Threshold=1800000000,
         ComparisonOperator='GreaterThanOrEqualToThreshold',
         TreatMissingData='missing',
         Tags=[]
